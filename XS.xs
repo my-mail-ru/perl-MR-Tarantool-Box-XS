my-mail-ru/perl-MR-IProto-XS

MY-8154 Use looks_like_number()

XS.xs
@@ -209,7 +209,7 @@ void iprotoxs_timeval_set(SV *sv, struct timeval *timeout) {
     if (SvIOK(sv)) {
         timeout->tv_sec = SvIV(sv);
         timeout->tv_usec = 0;
-    } else if (SvNOK(sv)) {
+    } else if (SvNOK(sv) || looks_like_number(sv)) {
         NV to = SvNV(sv);
         timeout->tv_sec = floor(to);
         timeout->tv_usec = floor((to - timeout->tv_sec) * 1000000);
@@ -222,7 +222,8 @@ void iprotoxs_parse_opts(iproto_message_opts_t *opts, HV *request) {
     SV **val;
 
     if ((val = hv_fetch(request, "shard_num", 9, 0))) {
-        if (!SvIOK(*val)) croak("Invalid \"shard_num\" value: \"%s\"", SvPV_nolen(*val));
+        if (!(SvIOK(*val) || looks_like_number(*val)))
+            croak("Invalid \"shard_num\" value: \"%s\"", SvPV_nolen(*val));
         opts->shard_num = SvUV(*val);
     }
 
@@ -271,7 +272,8 @@ void iprotoxs_parse_opts(iproto_message_opts_t *opts, HV *request) {
     }
 
     if ((val = hv_fetch(request, "max_tries", 9, 0))) {
-        if (!SvIOK(*val)) croak("\"max_tries\" should be an integer");
+        if (!(SvIOK(*val) || looks_like_number(*val)))
+            croak("\"max_tries\" should be an integer");
         opts->max_tries = SvIV(*val);
     }
 }
@@ -284,7 +286,8 @@ iproto_message_t *iprotoxs_hv_to_message(HV *request) {
 
     val = hv_fetch(request, "code", 4, 0);
     if (!val) croak("\"code\" should be specified");
-    if (!SvIOK(*val)) croak("Invalid \"code\" value");
+    if (!(SvIOK(*val) || looks_like_number(*val)))
+        croak("Invalid \"code\" value");
     code = SvUV(*val);
 
     val = hv_fetch(request, "request", 7, 0);

my-mail-ru/perl-MR-Tarantool-Box-XS

XS.xs
@@ -298,7 +298,8 @@ uint32_t tbxs_fetch_namespace(HV *request, tbns_t *ns) {
     } else {
         SV **val = hv_fetch(request, "namespace", 9, 0);
         if (!val) croak("\"namespace\" should be specified");
-        if (!SvIOK(*val)) croak("\"namespace\" should be an integer");
+        if (!(SvIOK(*val) || looks_like_number(*val)))
+            croak("\"namespace\" should be an integer");
         return SvUV(*val);
     }
 }
@@ -345,13 +346,15 @@ tarantoolbox_message_t *tbxs_select_hv_to_message(HV *request, tbns_t *ns, SV *e
 
     uint32_t offset = 0;
     if ((val = hv_fetch(request, "offset", 6, 0))) {
-        if (!SvIOK(*val)) croak("\"offset\" should be an integer");
+        if (!(SvIOK(*val) || looks_like_number(*val)))
+            croak("\"offset\" should be an integer");
         offset = SvUV(*val);
     }
 
     uint32_t limit = 0;
     if ((val = hv_fetch(request, "limit", 5, 0))) {
-        if (!SvIOK(*val)) croak("\"limit\" should be an integer");
+        if (!(SvIOK(*val) || looks_like_number(*val)))
+            croak("\"limit\" should be an integer");
         limit = SvUV(*val);
     }
 
@@ -894,7 +897,7 @@ ns_new(klass, ...)
                 ns->cluster = SvREFCNT_inc(SvRV(value));
                 has_cluster = true;
             } else if (strcmp(key, "namespace") == 0) {
-                if (!SvIOK(value))
+                if (!(SvIOK(value) || looks_like_number(value)))
                     croak("\"namespace\" should be an integer");
                 ns->namespace = SvUV(value);
                 has_namespace = true;